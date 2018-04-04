5bece4a3ef56511688723e51edf4c58bf256b49f
kittitorn.kan <kittitorn.kan@ascendcorp.com>
2018-04-04 22:54:47 +0700

    checkpoint2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header style={{height: '120px'}}>
+        <header style={{height: '80px'}}>
           <div style={{height: '80px'}}/>
           <div>
           <Link to='/'>Home</Link>
@@ -16,6 +16,29 @@ class App extends Component {
         <div className='container-fluid' style={{padding: '0', margin: '0'}}>
           <Route path='/' component={Home}/>
         </div>
+        <footer className='container-fluid' style={{textAlign: 'left' ,margin: '0', padding: '0'}}>
+          <div style={{backgroundColor: '#eee', margin: '0'}}>
+              <div style={{padding: '30px 0 50px 0'}}>
+                <div className='container custom-container' >
+                    <div className='col-sm-3'>
+                        <h4>Footer 1</h4>
+                        <p>Footer</p>
+                    </div>
+                    <div className='col-sm-3'>
+                        <h4>Footer 2</h4>
+                        <p>Footer</p>
+                    </div>
+                    <div className='col-sm-3'>
+                        <h4>Footer 3</h4>
+                        <p>Footer</p>
+                    </div>
+                    <div className='col-sm-3'>
+                        <h4>Footer 4</h4>
+                    </div>
+                </div>
+              </div>
+          </div>
+        </footer>
       </div>
     );
   }
--- src/component/Home/index.js
+++ src/component/Home/index.js
@@ -6,66 +6,61 @@ class Home extends Component {
     render() {
         return (
             <div className='Home'>
-            <div className='jumbotron' style={{margin: '0 0 0 0'}}>
+
+            <div className='jumbotron' style={{margin: '0 0 10 0'}}>
                 <h2>Home</h2>
                 <p>Home</p>
             </div>
-            <div className='container' style={{width: '768px'}}>
-                <div className='col-sm-3 thumbnail'>
-                    <img src={BikePic} className='img-rounded'/>
-                    <p>test</p>
+
+            <div style={{textAlign: 'left' ,margin: '0'}}>
+                <div className='container custom-container' >
+                    <div className='col-sm-3 thumbnail custom-thumbnail'>
+                        <img src={BikePic} className='img-rounded'/>
+                        <h4>IMG</h4>
+                        <p>test</p>
+                    </div>
+                    <div className='col-sm-3 thumbnail custom-thumbnail'>
+                        <img src={BikePic} className='img-rounded'/>
+                        <h4>IMG</h4>
+                        <p>test</p>
+                    </div>
+                    <div className='col-sm-3 thumbnail custom-thumbnail'>
+                        <img src={BikePic} className='img-rounded'/>
+                        <h4>IMG</h4>
+                        <p>test</p>
+                    </div>
+                    <div className='col-sm-3 thumbnail custom-thumbnail'>
+                        <img src={BikePic} className='img-rounded'/>
+                        <h4>IMG</h4>
+                        <p>test</p>
+                    </div>
                 </div>
-                <div className='col-sm-3 thumbnail'>
-                    <img src={BikePic} className='img-rounded'/>
-                    <p>test</p>
-                </div>
-                <div className='col-sm-3 thumbnail'>
-                    <img src={BikePic} className='img-rounded'/>
-                    <p>test</p>
-                </div>
-                <div className='col-sm-3 thumbnail'>
-                    <img src={BikePic} className='img-rounded'/>
-                    <p>test</p>
+                <div className='container custom-container' >
+                    <div className='col-sm-3 thumbnail custom-thumbnail'>
+                        <img src={BikePic} className='img-rounded'/>
+                        <h4>IMG</h4>
+                        <p>test</p>
+                    </div>
+                    <div className='col-sm-3 thumbnail custom-thumbnail'>
+                        <img src={BikePic} className='img-rounded'/>
+                        <h4>IMG</h4>
+                        <p>test</p>
+                    </div>
+                    <div className='col-sm-3 thumbnail custom-thumbnail'>
+                        <img src={BikePic} className='img-rounded'/>
+                        <h4>IMG</h4>
+                        <p>test</p>
+                    </div>
+                    <div className='col-sm-3 thumbnail custom-thumbnail'>
+                        <img src={BikePic} className='img-rounded'/>
+                        <h4>IMG</h4>
+                        <p>test</p>
+                    </div>
                 </div>
             </div>
-            <div className='container' style={{width: '768px'}}>
-                <div className='col-sm-3 thumbnail'>
-                    <img src={BikePic} className='img-rounded'/>
-                    <p>test</p>
-                </div>
-                <div className='col-sm-3 thumbnail'>
-                    <img src={BikePic} className='img-rounded'/>
-                    <p>test</p>
-                </div>
-                <div className='col-sm-3 thumbnail'>
-                    <img src={BikePic} className='img-rounded'/>
-                    <p>test</p>
-                </div>
-                <div className='col-sm-3 thumbnail'>
-                    <img src={BikePic} className='img-rounded'/>
-                    <p>test</p>
-                </div>
-            </div>
-            <footer>
-            <div className='container' style={{width: '768px'}}>
-                <div className='col-sm-3'>
-                    <h3>Footer 1</h3>
-                    <p>Footer</p>
-                </div>
-                <div className='col-sm-3'>
-                    <h3>Footer 2</h3>
-                    <p>Footer</p>
-                </div>
-                <div className='col-sm-3'>
-                    <h3>Footer 3</h3>
-                    <p>Footer</p>
-                </div>
-                <div className='col-sm-3'>
-                    <h3>Footer 4</h3>
-                    <p>Footer</p>
-                </div>
-                </div>
-            </footer>
+
+            
+
             </div>
             
         )
