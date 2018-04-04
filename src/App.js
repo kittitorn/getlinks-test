c03971065006eca8bb76501fe3564b55c2c1904d
kittitorn.kan <kittitorn.kan@ascendcorp.com>
2018-04-04 08:48:26 +0700

    Add Home & Nav header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Link, Route } from 'react-router-dom';
+import Home from './component/Home/';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header style={{height: '120px'}}>
+          <div style={{height: '80px'}}/>
+          <div>
+          <Link to='/'>Home</Link>
+          </div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <Route path='/' component={Home}/>
+        </div>
       </div>
     );
   }
